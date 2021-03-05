9ee275647fbb51645bcf2494c7a90adf031007a9
BBartels1 <bbartels1@eagles.ewu.edu>
2021-03-05 13:03:12 -0800

    fixed stuff i broke

--- TestProject/src/testPackage/Operations.java
+++ TestProject/src/testPackage/Operations.java
@@ -3,7 +3,7 @@ package testPackage;
 public class Operations{
 
   public int decrement(int x){ return x--; }
-  
-  public int doubleIncrement(int x){return (x-2)};
+  public int increment(int x) {return x++;}
+  public int doubleIncrement(int x){return (x-2);}
   
 }
