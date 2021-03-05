b81abf5f0e97b4a57a22dc6e650c1889788f9628
Savannah <80005725+sadams36@users.noreply.github.com>
2021-03-05 13:25:41 -0800

    Update Main.java

--- TestProject/src/testPackage/Main.java
+++ TestProject/src/testPackage/Main.java
@@ -1,13 +1,17 @@
 package testPackage;
 
-public class Main {
-	public static void main(String args[]) {
-		int i = 0; 
-      i = Operations.increment(i); 
-      System.out.println(i);
-      i = Operations.decrement(i); 
-      System.out.println(i);  
+public class Main{
+	
+	public static void main(String args[]){
+   
+	   int i = 0; 
+      
+	   i = Operations.increment(i); 
+	   System.out.println(i);
+      
+	   i = Operations.decrement(i); 
+	   System.out.println(i);  
+   
 	}
-
 
 }
