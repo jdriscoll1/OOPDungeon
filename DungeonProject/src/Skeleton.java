9452995392f1ee003740abcb46fa0971e09b6413
Savannah <80005725+sadams36@users.noreply.github.com>
2021-03-09 22:37:21 -0800

    Update Skeleton.java

--- DungeonProject/src/Skeleton.java
+++ DungeonProject/src/Skeleton.java
@@ -9,22 +9,31 @@
  * @version 1.0
  */
 
-public class Skeleton extends Monster
-{
+public class Skeleton extends Monster{
 
-    public Skeleton()
-	{
-		super("Sargath the Skeleton", 100, 3, .8, .3, 30, 50, 30, 50);
+	public Skeleton(){
+		
+		name = "Sargath the Skeleton";
+		
+		hitPoints = 100;
+		attackSpeed = 3;
+		
+		chanceToHit = .8;
+		chanceToHeal = .3;
+		
+		damageMin = 30; 
+		damageMax = 50;
+		
+		minHeal = 30; 
+		maxHeal = 50;
 
-    }//end constructor
+    }  // end constructor
 
-	public void attack(DungeonCharacter opponent)
-	{
-		System.out.println(name + " slices his rusty blade at " +
-							opponent.getName() + ":");
+	public void attack(DungeonCharacter opponent){
+		
+		System.out.println(name + " slices his rusty blade at " + opponent.getName() + ":");
 		super.attack(opponent);
 
-	}//end override of attack
+	}  // end override of attack
 
-
-}//end class Skeleton
+}  // end class Skeleton
