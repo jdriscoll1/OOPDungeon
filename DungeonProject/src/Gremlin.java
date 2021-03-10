701e0e853131fb31411cb8f3370bdbf2a84c80a7
Savannah <80005725+sadams36@users.noreply.github.com>
2021-03-09 22:32:22 -0800

    Update Gremlin.java

--- DungeonProject/src/Gremlin.java
+++ DungeonProject/src/Gremlin.java
@@ -9,22 +9,31 @@
  * @version 1.0
  */
 
-public class Gremlin extends Monster
-{
+public class Gremlin extends Monster{
 
-    public Gremlin()
-	{
-		super("Gnarltooth the Gremlin", 70, 5, .8, .4, 15, 30, 20, 40);
+    public Gremlin(){
+    	
+		name = "Gnarltooth the Gremlin";
+		
+		hitPoints = 70;
+		attackSpeed = 5;
+		
+		chanceToHit = .8;
+		chanceToHeal = .4;
+		
+		damageMin = 15; 
+		damageMax = 30;
+		
+		minHeal = 20; 
+		maxHeal = 40;
 
-    }//end constructor
+    }  // end constructor
 
-	public void attack(DungeonCharacter opponent)
-	{
-		System.out.println(name + " jabs his kris at " +
-							opponent.getName() + ":");
+	public void attack(DungeonCharacter opponent){
+		
+		System.out.println(name + " jabs his kris at " + opponent.getName() + ":");
 		super.attack(opponent);
 
-	}//end override of attack
+	}  // end override of attack
 
-
-}//end class Gremlin
+}  // end class Gremlin
